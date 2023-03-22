7d79bba65e806148d27b692760bf5275cbd035cb
Nadezhda Makhonina <122729471+MyakshinaNadi@users.noreply.github.com>
2023-03-23 02:14:34 +0300

    Add files via upload

---
+++ Проект. Модуль 6/task2/script.js
@@ -0,0 +1,17 @@
+const consoleLog = document.querySelector('#consoleLog');
+
+consoleLog.addEventListener('click', () => {
+   alert('Служит для вывода информации в косоль');
+});
+
+const Alert = document.querySelector('#alert');
+
+Alert.addEventListener('click', () => {
+    alert('Служит для вывода информации в косоль');
+});
+
+const Promt = document.querySelector('#prompt');
+
+Promt.addEventListener('click', () => {
+    alert('Служит для вывода информации в косоль');
+})
---
+++ Проект. Модуль 6/task4/script.js
@@ -0,0 +1,12 @@
+userTextField.addEventListener('click', function (event) { 
+    event.preventDefault(); 
+    const userText = prompt('Введите текст'); 
+    const userTextField = document.querySelector('#userTextField'); 
+    userTextField.textContent = userText; 
+});
+
+
+
+
+
+
